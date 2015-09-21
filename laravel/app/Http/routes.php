1235cb099f60202f4e877f03543f0a28682b33c7
Raul Palacios <palacios.raul02@gmail.com>
2015-09-21 18:19:01 -0500

    Primeras Rutas

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -8,8 +8,23 @@
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
-|
+| POST, GET, PUT, DELETE
 */
+Route::get('prueba',function(){
+	return "Hola desde routes.php";
+});
+
+Route::get('nombre/{nombre}',function($nombre){
+	return "Mi nombre es: ".$nombre;
+});
+
+Route::get('edad/{edad}',function($edad){
+	return "Mi edad es: ".$edad;
+});
+
+Route::get('edad2/{edad?}',function($edad = 20){
+	return "Mi edad es: ".$edad;
+});
 
 Route::get('/', function () {
     return view('welcome');
