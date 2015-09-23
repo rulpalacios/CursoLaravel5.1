1c8b78f5396e3fd1276537d48a3031759534ae00
Raul Palacios <palacios.raul02@gmail.com>
2015-09-22 19:25:20 -0500

    Codigo Optimizado

--- laravel/app/Http/Controllers/UsuarioController.php
+++ laravel/app/Http/Controllers/UsuarioController.php
@@ -10,8 +10,17 @@ use Cinema\Http\Controllers\Controller;
 use Cinema\User;
 use Session;
 use Redirect;
+use Illuminate\Routing\Route;
 class UsuarioController extends Controller
 {
+    public function __construct(){
+        $this->beforeFilter('@find',['only' => ['edit','update','destroy']]);
+    }
+
+    public function find(Route $route){
+        $this->user = User::find($route->getParameter('usuario'));
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -41,13 +50,9 @@ class UsuarioController extends Controller
      */
     public function store(UserCreateRequest $request)
     {
-        User::create([
-            'name' => $request['name'],
-            'email' => $request['email'],
-            'password' => $request['password'],
-        ]);
-        
-        return redirect('/usuario')->with('message','store');
+        User::create($request->all());
+        Session::flash('message','Usuario Creado Correctamente');
+        return Redirect::to('/usuario');
     }
 
     /**
@@ -69,8 +74,7 @@ class UsuarioController extends Controller
      */
     public function edit($id)
     {
-        $user = User::find($id);
-        return view('usuario.edit',['user'=>$user]);
+        return view('usuario.edit',['user'=>$this->user]);
     }
 
     /**
@@ -82,9 +86,8 @@ class UsuarioController extends Controller
      */
     public function update(UserUpdateRequest $request, $id)
     {
-        $user = User::find($id);
-        $user->fill($request->all());
-        $user->save();
+        $this->user->fill($request->all());
+        $this->user->save();
         Session::flash('message','Usuario Actualizado Correctamente');
         return Redirect::to('/usuario');
     }
@@ -97,8 +100,7 @@ class UsuarioController extends Controller
      */
     public function destroy($id)
     {
-        $user = User::find($id);
-        $user->delete();
+        $this->user->delete();
         Session::flash('message','Usuario Eliminado Correctamente');
         return Redirect::to('/usuario');
     }
--- laravel/resources/views/usuario/index.blade.php
+++ laravel/resources/views/usuario/index.blade.php
@@ -1,12 +1,5 @@
 @extends('layouts.admin')
-
-@if(Session::has('message'))
-<div class="alert alert-success alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  {{Session::get('message')}}
-</div>
-@endif
-
+	@include('alerts.success')
 	@section('content')
 	<table class="table">
 		<thead>
